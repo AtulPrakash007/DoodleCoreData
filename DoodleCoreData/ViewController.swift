7673159aba501cd39e440f08c6f7a8d428aae3b0
Mac <mac@Atul-MacBook-Pro.local>
2018-12-22 09:49:48 +0530

    Initial Commit

---
+++ DoodleCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DoodleCoreData
+//
+//  Created by Atul on 22/12/18.
+//  Copyright © 2018 Atul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
